e5112cc88efd47572e29dbcaeaccbd3f0eeb762d
luca-jt <random.lulu@die-genossen.de>
2025-05-23 21:46:15 +0200

    impl start + comments

--- ic3.cpp
+++ ic3.cpp
@@ -4,51 +4,55 @@
 
 
 bool ic3(const TS &ts) {
-  Frames frames;
-  newFrame(frames);
-  frames[0]; //wie konvertiert man eine expr zu einem Cube???
+  Cube init_cube(context);
+  init_cube.push_back(~ts.bad);
+  Cube bad_cube(context);
+  bad_cube.push_back(ts.bad);
+  std::vector<Frame> frames = { Frame{std::move(init_cube)} };
+  std::vector<Frame> badFrames = { Frame{std::move(bad_cube) }; // dass in c++ moven manuell gemacht werden muss ist schon echt bad
+
+  while (1) {
+    // checke Bedingungen von der aktuellen frame combi, falls ja return true
+    // sonst suche das CTI
+    // calle searchPathToInit() wo dann das CTI in den frames geblockt wird, falls der return hier false ist, return false (?)
+    // checke nach relative inductiveness
+    // erstelle neuen frame und bad frame mit der blocking clause -> hier generalize() verwenden?
+  }
+
   return true;
 }
 
 bool isInitial(const TS &ts, const Cube &cube) {
-  //die Implementierung wäre nur richtig, wenn in einem Cube nur ein State wäre
-  Cube cubesExprs(context);
-  cubesExprs.push_back(ts.init);
-  cubesExprs.push_back(expr(cube));
-  if (SAT(z3::mk_and(cubesExprs)) != std::nullopt) {
-    return true;
+  z3::expr conj = ts.init && expr(cube);
+  return SAT(conj).has_value(); // ich glaube das reicht (sollte zumindest äquivalent sein zu dem was davor da war), vielleicht kann man den SAT call vermeiden, indem man die expression nur vereinfacht und schaut ob sie false ist?
+  // ich glaube das sollte so auch gehen wenn 'cube' nicht nur ein zustand ist, sondern nur init enthält
+}
+
+std::optional<Cube> getBadInFrame(const TS &ts, const Frame &frame) {
+  z3::expr conj = ts.bad && expr(frame);
+  return SAT(conj);
+}
+
+bool searchPathToInit(const TS &ts, std::vector<Frame> &frames, Cube &cti) {
+  // TODO: Implement me
+}
+
+std::optional<Cube> getPre(const TS &ts, const std::vector<Frame> &frames, const Cube &cube, int relative_to) {
+  // TODO: Implement me
+}
+
+Cube generalize(const TS &ts, const std::vector<Frame> &frames, const Cube &cube, int frame_index) {
+  // TODO: Implement me
+}
+
+void blockCubeAtFrame(std::vector<Frame> &frames, int frame_index, const Cube &cube) {
+  Frame &frame = frames[frame_index];
+  for (auto &cube : frame) {
+    cube.push_back(~expr(cube)); // hier zB sollte man das auch einfach mit z3::expr verwenden können ohne cubes
   }
-  return false;
 }
 
-std::optional<Cube> getBad(const TS &ts, const Frame &frame) {
-  Cube cubesExprs(context);
-  cubesExprs.push_back(ts.bad);
-  cubesExprs.push_back(expr(frame));
-  return SAT(z3::mk_and(cubesExprs));
-}
-
-bool searchPathToInit(const TS &ts, Frames &frames, Cube &cti) {
-  // TODO: Implement me
-}
-
-std::optional<Cube> getPre(const TS &ts, const Frames &frames, const Cube &cube, int relativeTo) {
-  // TODO: Implement me
-}
-
-Cube generalize(const TS &ts, const Frames &frames, const Cube &cube, int frameIndex) {
-  // TODO: Implement me
-}
-
-void blockCubeAtFrame(Frames &frames, int frameIndex, const Cube &cube) {
-  // TODO: Implement me
-}
-
-bool isInductive(const TS &ts, const Frames &frames) {
-  // TODO: Implement me
-}
-
-void newFrame(Frames &frames) {
-  Frame new_frame;
-  frames.push_back(new_frame);
+bool isInductive(const TS &ts, const std::vector<Frame> &frames) {
+  const auto &frame = *frames.crbegin();
+  return isInvariant(ts, expr(frame));
 }
--- ic3.h
+++ ic3.h
@@ -5,7 +5,6 @@
 // IC3 algorithm
 
 typedef std::vector<Cube> Frame;
-typedef std::vector<Frame> Frames;
 
 // returns true if SAFE and false if UNSAFE
 bool ic3(const TS &ts);
@@ -18,21 +17,18 @@ std::optional<Cube> getBad(const TS &ts, const Frame &frame);
 
 // returns true if counterexample (a path to init) is found
 // returns false otherwise + strengthens frames to exclude path
-bool searchPathToInit(const TS &ts, Frames &frames, Cube &cti);
+bool searchPathToInit(const TS &ts, std::vector<Frame> &frames, Cube &cti);
 
 // returns predecessor of cube or nullopt if no predecessor exists
 // IC3 uses relative inductiveness
 // restrict the set of possible predecessors by overapproximation Fi and ~cube
-std::optional<Cube> getPre(const TS &ts, const Frames &frames, const Cube &cube, int relativeTo);
+std::optional<Cube> getPre(const TS &ts, const std::vector<Frame> &frames, const Cube &cube, int relative_to);
 
-// returns subcube that is still unreachable from init (disjoint with init + not reachable by previous overapproximation)
-Cube generalize(const TS &ts, const Frames &frames, const Cube &cube, int frameIndex);
+// returns subcube that is still unreachable from init (disjoint with init + not reachable by previous overapprox.)
+Cube generalize(const TS &ts, const std::vector<Frame> &frames, const Cube &cube, int frame_index);
 
 // strengthen frames by blocking cube at frame
-void blockCubeAtFrame(Frames &frames, int frameIndex, const Cube &cube);
+void blockCubeAtFrame(std::vector<Frame> &frames, int frame_index, const Cube &cube);
 
 // returns true if inductive and false otherwise
-bool isInductive(const TS &ts, const Frames &frames);
-
-// inserts new frame in frames
-void newFrame(Frames &frames);
+bool isInductive(const TS &ts, const std::vector<Frame> &frames);
